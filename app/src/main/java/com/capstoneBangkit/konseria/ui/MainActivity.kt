1d25c7ecead8a5b1b694c873e392e20bfb0460a5
christiansahlim <christiansahlim12@gmail.com>
2023-06-08 22:45:24 +0700

    fix MainActivity.kt & AndroidManifest.xml

---
+++ app/src/main/java/com/capstoneBangkit/konseria/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneBangkit.konseria.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.capstoneBangkit.konseria.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
